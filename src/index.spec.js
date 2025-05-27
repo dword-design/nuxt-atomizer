d9a66846e45a85ffe2899c7d8b505769c524ce3c
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-27 18:46:03 +0200

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,23 +2,26 @@ import { endent } from '@dword-design/functions';
 import { execaCommand } from 'execa';
 import nuxtDevReady from 'nuxt-dev-ready';
 import outputFiles from 'output-files';
-import { test } from 'playwright-local-tmp-dir';
 import kill from 'tree-kill-promise';
+import withLocalTmpDir from 'with-local-tmp-dir';
 
-test('minimal', async () => {
-  await outputFiles({
-    'pages/index.vue': endent`
-      <template>
-        <div />
-      </template>
-    `,
-  });
+export default {
+  minimal: () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        'pages/index.vue': endent`
+          <template>
+            <div />
+          </template>
+        `,
+      });
 
-  const nuxt = execaCommand('nuxt dev');
+      const nuxt = execaCommand('nuxt dev');
 
-  try {
-    await nuxtDevReady();
-  } finally {
-    await kill(nuxt.pid);
-  }
-});
+      try {
+        await nuxtDevReady();
+      } finally {
+        await kill(nuxt.pid);
+      }
+    }),
+};
