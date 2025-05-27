40ec1df61b98a2bd9b4f031ab5955e292bf60a64
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-27 19:12:27 +0200

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,17 @@
 import { endent } from '@dword-design/functions';
+import { test } from '@playwright/test';
 import { execaCommand } from 'execa';
 import nuxtDevReady from 'nuxt-dev-ready';
 import outputFiles from 'output-files';
-import { test } from 'playwright-local-tmp-dir';
+import withLocalTmpDir from 'with-local-tmp-dir';
+
+let reset;
+test.beforeEach(async () => (reset = await withLocalTmpDir()));
+test.afterEach(() => reset());
 
 test('minimal', async () => {
   await outputFiles({
+    //'nuxt.config.js': "export default { modules: ['foo'] }",
     'pages/index.vue': endent`
       <template>
         <div />
