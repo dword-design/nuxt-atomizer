d6c21c7ae272d2a6513fe5d1a0abe6e093bd2d5e
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-28 15:58:28 +0200

    fix: foo

--- index.spec.js
+++ index.spec.js
@@ -1,24 +1,3 @@
-import dedent from 'dedent';
-import { execaCommand } from 'execa';
-import nuxtDevReady from 'nuxt-dev-ready';
-import kill from 'tree-kill-promise';
-import fs from 'fs-extra';
+import run from './run.js';
 
-it('valid', async () => {
-  await fs.outputFile('pages/index.vue', dedent`
-    <template>
-      <div />
-    </template>
-  `);
-
-  const nuxt = execaCommand('nuxt dev', { stdio: 'inherit' });
-
-  try {
-    await nuxtDevReady();
-    await new Promise(resolve => setTimeout(resolve, 1000));
-  } finally {
-    await kill(nuxt.pid);
-    await execaCommand('nuxi cleanup');
-    await fs.remove('pages/index.vue');
-  }
-});
+it('valid', () => run());
---
+++ run.js
@@ -0,0 +1,24 @@
+import dedent from 'dedent';
+import { execaCommand } from 'execa';
+import nuxtDevReady from 'nuxt-dev-ready';
+import kill from 'tree-kill-promise';
+import fs from 'fs-extra';
+
+export default async () => {
+  await fs.outputFile('pages/index.vue', dedent`
+    <template>
+      <div />
+    </template>
+  `);
+
+  const nuxt = execaCommand('nuxt dev', { stdio: 'inherit' });
+
+  try {
+    await nuxtDevReady();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  } finally {
+    await kill(nuxt.pid);
+    await execaCommand('nuxi cleanup');
+    await fs.remove('pages/index.vue');
+  }
+};
