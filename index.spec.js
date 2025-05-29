081b94f85c6ce367ce27f56bb66b0faf8ca98440
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-29 02:42:22 +0200

    chore: foo

--- index.spec.js
+++ index.spec.js
@@ -1,21 +1,23 @@
 import dedent from 'dedent';
-import { execa } from 'execa';
+import { execaCommand } from 'execa';
 import nuxtDevReady from 'nuxt-dev-ready';
 import kill from 'tree-kill-promise';
 import fs from 'fs-extra';
 import { test } from 'playwright-local-tmp-dir';
 
-test('valid', async () => {
+test('valid', async ({ page }) => {
+  test.setTimeout(60_000);
   await fs.outputFile('pages/index.vue', dedent`
     <template>
-      <div>Hellow world</div>
+      <div>Hello world</div>
     </template>
   `);
 
-  let nuxt = execa('nuxt dev --no-fork', { reject: false, stdio: 'inherit' });
+  let nuxt = execaCommand('nuxt dev --no-fork', { reject: false, stdio: 'inherit' });
 
   try {
     await nuxtDevReady();
+    await page.goto(`http://localhost:3000`);
     await new Promise(resolve => setTimeout(resolve, 1000));
   } finally {
     await kill(nuxt.pid);
