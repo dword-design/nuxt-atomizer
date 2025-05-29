89a0ab3a4be9f85bae0fd8f0c4a0a16c25e6493d
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-29 03:00:03 +0200

    fix: foo

--- index.spec.js
+++ index.spec.js
@@ -3,21 +3,23 @@ import { execaCommand } from 'execa';
 import nuxtDevReady from 'nuxt-dev-ready';
 import kill from 'tree-kill-promise';
 import fs from 'fs-extra';
-import { test } from 'playwright-local-tmp-dir';
+import { test } from '@playwright/test';
+import pathLib from 'node:path';
 
-test('valid', async () => {
+test('valid', async ({ page }, testInfo) => {
   test.setTimeout(60_000);
-  await fs.outputFile('pages/index.vue', dedent`
+  const tmpDir = testInfo.outputPath('');
+  await fs.outputFile(pathLib.join(tmpDir, 'pages', 'index.vue'), dedent`
     <template>
       <div>Hello world</div>
     </template>
   `);
 
-  let nuxt = execaCommand('nuxt dev --no-fork', { reject: false, stdio: 'inherit' });
+  let nuxt = execaCommand('nuxt dev --no-fork', { reject: false, stdio: 'inherit', cwd: tmpDir });
 
   try {
     await nuxtDevReady();
-    //await page.goto(`http://localhost:3000`);
+    await page.goto(`http://localhost:3000`);
     await new Promise(resolve => setTimeout(resolve, 1000));
   } finally {
     await kill(nuxt.pid);
